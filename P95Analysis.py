0c3390f108a80186a029073865b798928ca1ac5d
Jorge <jogonzal@users.noreply.github.com>
2019-09-15 16:31:47 -0700

    Add case with caching

--- P95Analysis.py
+++ P95Analysis.py
@@ -5,14 +5,25 @@ import numpy as np
 from matplotlib import mlab
 
 
-def getNumbersFromCappedNormalDistribution(median, stdev, minimum, count):
+def getNumbersFromCappedNormalDistribution(median, stdev, minimum, count, cacheHitRate, cacheGetSetDurationDuration):
     accumulatedFilteredNumbers = []
     while True:
         numbers = np.random.normal(median, stdev, count - len(accumulatedFilteredNumbers))
         filteredNumbers = list(filter(lambda n: n > minimum, numbers))
         accumulatedFilteredNumbers.extend(filteredNumbers)
         if len(accumulatedFilteredNumbers) == count:
-            return accumulatedFilteredNumbers
+            break
+    # now, we iterate and apply the cache hit rate
+    for i in range(len(accumulatedFilteredNumbers)):
+        number = accumulatedFilteredNumbers[i]
+        cacheDuration = max(0.1, np.random.normal(cacheGetSetDurationDuration, cacheGetSetDurationDuration * 2))
+        if np.random.random() < cacheHitRate:
+            # cache hit
+            number = max(0.1, cacheDuration)
+        else:
+            number = number + cacheDuration
+        accumulatedFilteredNumbers[i] = number
+    return accumulatedFilteredNumbers
 
 # Add 2 arrays
 def add2Arrays(list1, list2):
@@ -39,15 +50,15 @@ def showHistogram(list, title):
 
 def runSampleWithoutCaching():
     numSamples = 100000
-    durationsForApiCall1 = getNumbersFromCappedNormalDistribution(2, 1, 0.5, numSamples)
+    durationsForApiCall1 = getNumbersFromCappedNormalDistribution(2, 1, 0.5, numSamples, 0, 0)
     plt.subplot(221)
     showHistogram(durationsForApiCall1, 'API call 1')
 
-    durationsForApiCall2 = getNumbersFromCappedNormalDistribution(2, 1.3, 0.4, numSamples)
+    durationsForApiCall2 = getNumbersFromCappedNormalDistribution(2, 1.3, 0.4, numSamples, 0, 0)
     plt.subplot(222)
     showHistogram(durationsForApiCall2, 'API call 2')
 
-    durationsForApiCall3 = getNumbersFromCappedNormalDistribution(1, 0.7, 0.3, numSamples)
+    durationsForApiCall3 = getNumbersFromCappedNormalDistribution(1, 0.7, 0.3, numSamples, 0, 0)
     plt.subplot(223)
     showHistogram(durationsForApiCall3, 'API call 3')
 
@@ -56,4 +67,24 @@ def runSampleWithoutCaching():
     showHistogram(totalDuration, 'Total')
     plt.show()
 
+def runSampleWithCaching():
+    numSamples = 100000
+    durationsForApiCall1 = getNumbersFromCappedNormalDistribution(2, 1, 0.5, numSamples, 0.3, 0.2)
+    plt.subplot(221)
+    showHistogram(durationsForApiCall1, 'API call 1 (with caching)')
+
+    durationsForApiCall2 = getNumbersFromCappedNormalDistribution(2, 1.3, 0.4, numSamples, 0.3, 0.2)
+    plt.subplot(222)
+    showHistogram(durationsForApiCall2, 'API call 2 (with caching)')
+
+    durationsForApiCall3 = getNumbersFromCappedNormalDistribution(1, 0.7, 0.3, numSamples, 0.3, 0.2)
+    plt.subplot(223)
+    showHistogram(durationsForApiCall3, 'API call 3 (with caching)')
+
+    totalDuration = add2Arrays(add2Arrays(durationsForApiCall1, durationsForApiCall2), durationsForApiCall3)
+    plt.subplot(224)
+    showHistogram(totalDuration, 'Total (with caching)')
+    plt.show()
+
 runSampleWithoutCaching()
+runSampleWithCaching()
